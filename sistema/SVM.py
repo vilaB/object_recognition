b7f1e60c70bc62e1eca7c42de48a35b1d0eb2d36
Gabriel Vilariño Besteiro <gabrivb@outlook.es>
2022-03-26 11:47:09 +0100

    Normalizo con método

--- sistema/SVM.py
+++ sistema/SVM.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-from sklearn import svm
+from sklearn import svm, calibration
+from sklearn.preprocessing import StandardScaler
 
 class SVMOpenCV:
     svm = None
@@ -20,23 +21,27 @@ class SVMOpenCV:
     
 class SVM:
     svm = None
+    scaler = None
     
     def __init__(self, muestra: list, etiquetas: list) -> None:
         self.svm = svm.LinearSVC()
-        self.svm = self.svm.fit(muestra, etiquetas)
+        self.scaler = StandardScaler()
+        self.scaler = self.scaler.fit(muestra)
+        self.svm = calibration.CalibratedClassifierCV(self.svm)
+        self.svm = self.svm.fit(self.scaler.transform(muestra), etiquetas.ravel())
     
 
     def procesar_imagen(self, secuencia: list) -> float:
-        scores = self.svm.decision_function(secuencia)      # Confidence scores per (n_samples, n_classes) combination. In the binary case, confidence score for self.classes_[1] where >0 means this class would be predicted.
-        w_norm = np.linalg.norm(self.svm.coef_)
-        dist = scores / w_norm                              # Normalizada         
-        return -dist
+        secuencia = self.scaler.transform(secuencia)
+        scores = self.svm.predict_proba(secuencia)[:,0]      # Confidence scores per (n_samples, n_classes) combination. In the binary case, confidence score for self.classes_[1] where >0 means this class would be predicted.        
+        scores = scores - 0.5
+        return -scores
 
 
 
 if __name__ == "__main__":
-    muestra = np.array([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]])
-    etiquetas = np.array([0,0,0,0,0,1,1,1])
+    muestra = np.array([[0,0,0], [0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1], [1,1,1], [1,1,1]])
+    etiquetas = np.array([0, 0,0,0,0,0,1,1,1,1,1])
 
     clasificador = SVM(muestra, etiquetas)
     print(clasificador.procesar_imagen(muestra))
