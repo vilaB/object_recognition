032b4b98184a3c0f4af58a6b5660d044f1779908
Gabriel Vilariño Besteiro <gabrivb@outlook.es>
2022-07-31 22:57:52 +0200

    Procesado auto

--- procesar_resultados/main.sh
+++ procesar_resultados/main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd ../experimentos/$1
-touch ../procesar_resultados/nosup_$1.txt
+touch ../../procesar_resultados/nosup_$1.txt
 for file in *resultados_nosup.txt; 
 do
     head -n 1 $file
     head -n 1 $file >> ../procesar_resultados/nosup_$1.txt
 done
 
-touch ../procesar_resultados/sup_$1.txt
+touch ../../procesar_resultados/sup_$1.txt
 for file in *resultados_sup.txt; 
 do
     head -n 1 $file
